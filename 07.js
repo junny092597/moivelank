0ee1ae78b981346a7e9104393681ae77db1b5b6b
bokchi0925 <146904515+junny092597@users.noreply.github.com>
2023-10-24 14:31:47 +0900

    Update 07.js

--- 07.js
+++ 07.js
@@ -1,17 +1,1 @@
-function add (input) {
 
-}
-
-//2개의 숫자를 받아서 덧셈을 한 후 내보내는 함수
-function add(x, y) {
-    return x + y;
-}
-
-//2. 함수 표현식
-let add2 = function (x, y) {
-    return x + y;
-};
-
-//3. 함수를 호출한다(= 사용한다)
-// 함수명() add(입력값)
-console.log(add(2,3));
